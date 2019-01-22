6135d3084bf0e0a4932f00d6b455bbfdef889e4b
Robin Randhawa <robin.randhawa@arm.com>
2019-01-22 14:56:13 +0000

    aarch64-prep: Pull in the bitfield crate
    Used by aarch64 code.

--- src/lib.rs
+++ src/lib.rs
@@ -34,6 +34,8 @@ extern crate alloc;
 
 #[macro_use]
 extern crate bitflags;
+#[macro_use]
+extern crate bitfield;
 extern crate goblin;
 extern crate linked_list_allocator;
 extern crate rustc_demangle;
