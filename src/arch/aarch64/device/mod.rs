025d72ae51064c8658259f60217e2fea21f64706
Robin Randhawa <robin.randhawa@arm.com>
2019-02-15 21:05:29 +0000

    aarch64: Flesh out device::init

--- src/arch/aarch64/device/mod.rs
+++ src/arch/aarch64/device/mod.rs
@@ -1,2 +1,20 @@
+use paging::ActivePageTable;
+
 pub mod cpu;
+pub mod gic;
+pub mod generic_timer;
 pub mod serial;
+pub mod rtc;
+
+pub unsafe fn init(_active_table: &mut ActivePageTable) {
+    gic::init();
+    generic_timer::init();
+}
+
+pub unsafe fn init_noncore() {
+    serial::init();
+    rtc::init();
+}
+
+pub unsafe fn init_ap() {
+}
