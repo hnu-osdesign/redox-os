b446cba41bef6bc868b44ca028f7347ef6db259b
Robin Randhawa <robin.randhawa@arm.com>
2019-02-15 21:04:04 +0000

    aarch64: Flesh out kreset and kstop
    Note that these use asm code to invoke PSCI calls. Ideally we should
    have a PSCI crate that we can call into. That's for later.

--- src/arch/aarch64/stop.rs
+++ src/arch/aarch64/stop.rs
@@ -1,4 +1,21 @@
 #[no_mangle]
+pub unsafe extern fn kreset() -> ! {
+    println!("kreset");
+
+    let val: u32 = 0x8400_0009;
+    asm!("mov   x0, $0" : : "r"(val) : : "volatile");
+    asm!("hvc   #0" : : : : "volatile");
+
+    unreachable!();
+}
+
+#[no_mangle]
 pub unsafe extern fn kstop() -> ! {
-    loop {}
+    println!("kstop");
+
+    let val: u32 = 0x8400_0008;
+    asm!("mov   x0, $0" : : "r"(val) : : "volatile");
+    asm!("hvc   #0" : : : : "volatile");
+
+    unreachable!();
 }
