6f9d04d3b58fe2c86c814c219258dc319f97f0d0
Robin Randhawa <robin.randhawa@arm.com>
2019-03-12 00:30:56 +0000

    aarch64: Make COM1 initialization dynamic
    The Mutex guarding the SerialPort instance now guards an Option instead.
    That option is populated by initialization code introduced here that
    probes the device tree for a diagnostic UART advertised in the chosen
    node.

--- src/arch/aarch64/debug.rs
+++ src/arch/aarch64/debug.rs
@@ -5,19 +5,24 @@ use devices::uart_pl011::SerialPort;
 use super::device::serial::COM1;
 
 pub struct Writer<'a> {
-    serial: MutexGuard<'a, SerialPort<Pio<u8>>>,
+    serial: MutexGuard<'a, Option<SerialPort>>,
 }
 
 impl<'a> Writer<'a> {
     pub fn new() -> Writer<'a> {
         Writer {
-            serial: COM1.lock(),
+            serial: unsafe { COM1.lock() },
         }
     }
 }
 
 impl<'a> fmt::Write for Writer<'a> {
     fn write_str(&mut self, s: &str) -> Result<(), fmt::Error> {
-        self.serial.write_str(s)
+        if let Some(ref mut serial_port) = *self.serial {
+            serial_port.write_str(s);
+            Ok(())
+        } else {
+            Err(fmt::Error)
+        }
     }
 }
